f35ef31c2db87115d11fd6d1e113e4bb85100884
DzmitryMytsko <dzmitrymytsko@gmail.com>
2023-10-05 23:17:26 +0300

    Corrected Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -24,7 +24,7 @@ void PrintArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length - 1; i++)
     {
-        Console.Write($"{array[i]},");
+        Console.Write($"{array[i]}, ");
     }
     Console.Write($"{array[array.Length - 1]}");
     Console.Write("] -> ");
